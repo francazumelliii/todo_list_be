0a7b510c618159ec88e70a550f9ca0b1ca437ac0
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-12-13 14:20:16 +0100

    fix CORS

---
+++ src/main/java/it/jac/project_work/todo_list_be/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package it.jac.project_work.todo_list_be.security;
+
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
